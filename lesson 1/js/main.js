484ce780a3af81b6b3bc5735a2c29d95c074bf0a
Taras <tarasvitruk.v@gmail.com>
2018-05-30 15:38:29 +0200

    First lesson

---
+++ lesson 1/js/main.js
@@ -0,0 +1,17 @@
+var money = prompt("Ваш бюджет на месяц?", "50000");
+var nameShop = prompt("Название вашего магазина?");
+
+var mainList = {
+	money,
+	nameShop,
+	shopGoods: [],
+};
+console.log(mainList);
+
+
+var shopGoods = prompt("Какой тип товаров будем продавать?");
+var shopGoods = prompt("Какой тип товаров будем продавать?");
+var shopGoods = prompt("Какой тип товаров будем продавать?");
+
+console.log(mainList);
+
