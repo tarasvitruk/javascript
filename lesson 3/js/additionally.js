c6744833a5b4e7096f89511240c7aad541aaf1bf
Taras <tarasvitruk.v@gmail.com>
2018-06-02 18:35:35 +0200

    lesson 3 fixed

--- lesson 3/js/additionally.js
+++ lesson 3/js/additionally.js
@@ -1,21 +1,27 @@
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-for (var i = 0; i < week.length; i++) {
-	if (i > 4) {
-			document.write(week[i].bold() + '<br />' );
-	  }
-	else if (i == 3) {
-			document.write(week[i].italics() + '<br />' );
-	  }
-	else {
-		document.write(week[i] + '<br />');
-	}
-} 
+let str = "урок-3-был слишком легким";
+let newStr = str[0].toUpperCase() + str.slice(1).replace( /-/g, " " );
+		console.log(newStr);
+		newStrTwo = newStr.replace( "легким", "" );
+		newStrThree = newStrTwo.replace( "слишком", "слишкоо" );
+		alert(newStrThree);
 
-
-
-let arr = ['316', '198', '777', '490', '511', '374', '200'];
-for (var i = 0; i < arr.length; i++) {
-	if (arr[i][0] == 3 || arr[i][0] == 7) {
-		console.log(arr[i]);
-	}
+let arr = [20, 33, 1, "Человек", 2, 3];
+let cubeSum = 0;
+for (let i = 0; i < 6; i++) {
+	if((typeof(arr[i])) === 'number') {
+		var arr3 = arr[i]**3;
+		cubeSum += arr3;
+	} else {
+			console.log('Не в этот раз =)');
+		}
 }
+console.log(Math.sqrt(cubeSum));
+
+
+
+// function lesson(str) {
+// 	if (!str) return str;
+
+// 	return str[0].toUpperCase() + str.slice(1);
+// }
+// alert(newStr);
