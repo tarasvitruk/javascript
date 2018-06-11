7f21f11a850143e9d603008f3ba83df47cc1c6fc
Taras <tarasvitruk.v@gmail.com>
2018-06-11 15:53:21 +0200

    lesson 4 additionally

--- lesson 4/task/exam/index.js
+++ lesson 4/task/exam/index.js
@@ -1,48 +1,47 @@
 function getFriendlyNumbers(start, end) {
-	let arrN = [];
+	let friendlyNumbersArr = [];
 		if ((typeof(start)) === 'number' && (typeof(end)) === 'number' && (start ^ 0) === start && (end ^ 0) === end && end >= start && start > 0 && end > 0 ){
 
 			for (let i = start; i <= end; i++) {
 	    	for (let y = start; y <= end; y++) {
 	      	if (FrendlyNamber(i, y) && i !== y && i > y) {
-	       	 arrN.push([i, y].sort());
+	       	 friendlyNumbersArr.push([i, y].sort());
 	      	}
 	    	}
 	  	}
 		} else {
 			 return false;
 		};
-	return arrN;
-	//console.log(arrN)
+	return friendlyNumbersArr;
 }
 getFriendlyNumbers(1, 300);
 
 
-        function FrendlyNamber(i, y) {
-            if (getDivisorsSum(i) == y && getDivisorsSum(y) == i) { return true; } 
-        }
+function FrendlyNamber(i, j) {
+    if (getDivisorsSum(i) == j && getDivisorsSum(j) == i) { return true; } 
+}
 
-        function SumOfTheArray(arr) { 
-            var sum = 0;
-            for(let i = 0; i < arr.length; i++) {
-                sum = sum + arr[i];
-            }
-            return sum;
-        }
+function SumOfTheArray(arr) { 
+    var sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
 
-        function Divisors(num){ 
-            var arr = [];
-            for(let i = 1; i < num; i++) {
-                if(num % i === 0){
-                    arr.push(i);
-                }
-            }
-            return(arr)
+function Divisors(num){ 
+    var arr = [];
+    for(let i = 1; i < num; i++) {
+        if(num % i === 0){
+            arr.push(i);
         }
+    }
+    return(arr)
+}
 
-        function getDivisorsSum(num) {
-            return SumOfTheArray(Divisors(num));
-        }
+function getDivisorsSum(num) {
+    return SumOfTheArray(Divisors(num));
+}
 	
 
 
