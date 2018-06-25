b49e571c0cd01600ed73987ba9fb9d31349ca270
Taras <tarasvitruk.v@gmail.com>
2018-06-25 23:27:28 +0200

    lesson 10 ad

--- lesson_10/additionally/js/main.js
+++ lesson_10/additionally/js/main.js
@@ -1,36 +1,11 @@
-let btn = document.querySelector('.btn'),
-		box = document.querySelector('.box');
+let input = document.getElementById('tel');
 
-function animate({timing, draw, duration}) {
+input.addEventListener('focus', n => {
+	if(!/^\+\d*$/.test(input.value))
+		input.value = '+7';
+});
 
-  let start = performance.now();
-
-  requestAnimationFrame(function animate(time) {
-    
-    let timeFraction = (time - start) / duration;
-    if (timeFraction > 1) timeFraction = 1;
-
-    
-    let progress = timing(timeFraction);
-
-    draw(progress); 
-
-    if (timeFraction < 1) {
-      requestAnimationFrame(animate);
-    }
-
-  });
-}
-
-function myAnimations() {
-      animate({
-        duration: 3000,
-        timing: function elastic(x, timeFraction) {
-          return Math.pow(2, 10 * (timeFraction - 1)) * Math.cos(20 * Math.PI * x / 3 * timeFraction)
-        }.bind(null, 1.5),
-        draw: function(progress) {
-          box.style.left = progress * 500 + 'px';
-        }
-      });
-};
-btn.addEventListener('click', myAnimations);
+input.addEventListener('keypress', a => {
+	if(!/\d/.test(a.key))
+		a.preventDefault();
+});
